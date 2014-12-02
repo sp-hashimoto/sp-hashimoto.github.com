2d6f4d98c69364a5546d30d003aad6ffa9ebd46e
masahikoy53 <git.yamada@gmail.com>
2014-12-02 15:51:12 +0900

    ５に戻す

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'hashimoto_2014_budget_6';
+OpenSpending.identifier = 'hashimoto_2014_budget_5';
 OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
